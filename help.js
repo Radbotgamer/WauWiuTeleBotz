c96145b78de066f7ad523589775a1358dc3ff45a
Radbotgamer <98032766+Radbotgamer@users.noreply.github.com>
2023-12-31 17:17:29 +0700

    help.js

--- help.js
+++ help.js
@@ -15,7 +15,7 @@ function toCommas(x) {
 exports.allMenu = (role, ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isCreator, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
     return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
 
-Hitung Mundur Idul Fitri 🌙 
+Umur WauWiuBotz-MD 
 ${mundur}
 
 *STATISTICS*
